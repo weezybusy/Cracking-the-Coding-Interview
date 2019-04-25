08525c689e343aaaf45155e44da783a6db1d3b49
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2019-04-25 15:33:43 +0300

    Finished tests for check_permutation(). Added cosmetics to test_is_unique.py.

--- Problems/check_permutation/test_check_permutation.py
+++ Problems/check_permutation/test_check_permutation.py
@@ -1,27 +1,65 @@
 #!/usr/bin/env python3
 
 import unittest
-import check_permutation
+import check_permutation as cp
 
 
 class TestCheckPermutation(unittest.TestCase):
 
     def test_check_permutation_sort(self):
-        self.assertTrue(check_permutation.check_permutation_sort('', ''))
-        self.assertFalse(check_permutation.check_permutation_sort('hell', 'olleh'))
-        self.assertFalse(check_permutation.check_permutation_sort('hello', 'olle'))
-        self.assertTrue(check_permutation.check_permutation_sort('hello', 'olleh'))
-        self.assertFalse(check_permutation.check_permutation_sort('hello', 'olllh'))
-        self.assertFalse(check_permutation.check_permutation_sort('hlllo', 'olleh'))
+        # Positive tests.
+        self.assertTrue(cp.check_permutation_sort('', ''))
+        self.assertTrue(cp.check_permutation_sort('a', 'a'))
+        self.assertTrue(cp.check_permutation_sort('ab', 'ab'))
+        self.assertTrue(cp.check_permutation_sort('ab', 'ba'))
+        self.assertTrue(cp.check_permutation_sort('ba', 'ab'))
+        self.assertTrue(cp.check_permutation_sort('abc', 'abc'))
+        self.assertTrue(cp.check_permutation_sort('abc', 'acb'))
+        self.assertTrue(cp.check_permutation_sort('abc', 'cab'))
+        self.assertTrue(cp.check_permutation_sort('abc', 'cba'))
+        self.assertTrue(cp.check_permutation_sort('abc', 'bca'))
+        self.assertTrue(cp.check_permutation_sort('abc', 'bac'))
+        self.assertTrue(cp.check_permutation_sort('aac', 'aca'))
+        self.assertTrue(cp.check_permutation_sort('aaaa', 'aaaa'))
+
+        # Negative tests.
+        self.assertFalse(cp.check_permutation_sort('', 'a'))
+        self.assertFalse(cp.check_permutation_sort('a', ''))
+        self.assertFalse(cp.check_permutation_sort('ab', 'a'))
+        self.assertFalse(cp.check_permutation_sort('a', 'ab'))
+        self.assertFalse(cp.check_permutation_sort('aa', 'ab'))
+        self.assertFalse(cp.check_permutation_sort('abc', 'aabc'))
+        self.assertFalse(cp.check_permutation_sort('aabc', 'abc'))
+        self.assertFalse(cp.check_permutation_sort('abc', 'aac'))
+        self.assertFalse(cp.check_permutation_sort('ccc', 'cac'))
+
 
     def test_check_permutation_hash(self):
-        self.assertTrue(check_permutation.check_permutation_hash('', ''))
-        self.assertFalse(check_permutation.check_permutation_hash('hell', 'olleh'))
-        self.assertFalse(check_permutation.check_permutation_hash('hello', 'olle'))
-        self.assertTrue(check_permutation.check_permutation_hash('hello', 'olleh'))
-        self.assertFalse(check_permutation.check_permutation_hash('hello', 'olllh'))
-        self.assertFalse(check_permutation.check_permutation_hash('hlllo', 'olleh'))
-        self.assertTrue(check_permutation.check_permutation_hash('hello', 'hello'))
+        # Positive tests.
+        self.assertTrue(cp.check_permutation_hash('', ''))
+        self.assertTrue(cp.check_permutation_hash('a', 'a'))
+        self.assertTrue(cp.check_permutation_hash('ab', 'ab'))
+        self.assertTrue(cp.check_permutation_hash('ab', 'ba'))
+        self.assertTrue(cp.check_permutation_hash('ba', 'ab'))
+        self.assertTrue(cp.check_permutation_hash('abc', 'abc'))
+        self.assertTrue(cp.check_permutation_hash('abc', 'acb'))
+        self.assertTrue(cp.check_permutation_hash('abc', 'cab'))
+        self.assertTrue(cp.check_permutation_hash('abc', 'cba'))
+        self.assertTrue(cp.check_permutation_hash('abc', 'bca'))
+        self.assertTrue(cp.check_permutation_hash('abc', 'bac'))
+        self.assertTrue(cp.check_permutation_hash('aac', 'aca'))
+        self.assertTrue(cp.check_permutation_hash('aaaa', 'aaaa'))
+
+        # Negative tests.
+        self.assertFalse(cp.check_permutation_hash('', 'a'))
+        self.assertFalse(cp.check_permutation_hash('a', ''))
+        self.assertFalse(cp.check_permutation_hash('ab', 'a'))
+        self.assertFalse(cp.check_permutation_hash('a', 'ab'))
+        self.assertFalse(cp.check_permutation_hash('aa', 'ab'))
+        self.assertFalse(cp.check_permutation_hash('abc', 'aabc'))
+        self.assertFalse(cp.check_permutation_hash('aabc', 'abc'))
+        self.assertFalse(cp.check_permutation_hash('abc', 'aac'))
+        self.assertFalse(cp.check_permutation_hash('ccc', 'cac'))
 
 
 if __name__ == '__main__':
