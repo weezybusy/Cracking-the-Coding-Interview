36cd4ed3f6b0d964ed3614420525df2f85812091
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2019-06-05 12:07:29 +0300

    In process of solving Rotate Matrix problem.

---
+++ Problems/rotate_matrix/rotate_matrix.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+"""
+Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in
+the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you
+do this in place?
+"""
+
+# Check if the provided matrix is NxN.
+def is_nxn(matrix):
+    n = len(matrix)
+    if n == 0:
+        return False
+    for i in range(n):
+        if len(matrix[i]) != n:
+            return False
+    return True
+
+
+# Time complexity:  O(n^2).
+# Space complexity: O(n).
+def rotate_matrix(matrix):
+
+    if not is_nxn(matrix):
+        return None
+
+    n = len(matrix)
+    new_matrix = [[0 for j in range(n)] for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            new_matrix[i][j] = matrix[j][n-1-i]
+    return new_matrix
+
+
+# Time complexity:  O(n^2).
+# Space complexity: O(1).
+def rotate_matrix_in_place(matrix):
+
+    if not is_nxn(matrix):
+        return False
+
+    n = len(matrix)
+    for layer in range(n/2):
+        first = layer
+        last = n - 1 - layer
+        for i in range(first, last):
+            offset = i - first
+
+            # Save top.
+            top = matrix[first][i]
+
+            # left -> top.
+            matrix[first][i] = matrix[last-offset][first]
+
+            # bottom -> left.
+            matrix[last-offset][first] = matrix[last][last-offset]
+
+            # right -> bottom.
+            matrix[last][last-offset] = matrix[i][last]
+
+            # top -> right.
+            matrix[i][last] = top
+    return True
