d380d532537b0ea892e94414101e5ad077e7d738
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2019-06-09 16:59:17 +0300

    Added alternate solution and tests for Zero Matrix problem.

--- Problems/zero_matrix/test_zero_matrix.py
+++ Problems/zero_matrix/test_zero_matrix.py
@@ -34,6 +34,33 @@ class TestZeroMatrix(unittest.TestCase):
         expected_output = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
         self.assertEqual(matrix, expected_output)
 
+    def test_zero_matrix_alt(self):
+
+        matrix = [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
+        expected_output = [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
+        zm.zero_matrix_alt(matrix)
+        self.assertEqual(matrix, expected_output)
+
+        matrix = [[1, 0, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
+        zm.zero_matrix_alt(matrix)
+        expected_output = [[0, 0, 0], [1, 0, 1], [1, 0, 1], [1, 0, 1]]
+        self.assertEqual(matrix, expected_output)
+
+        matrix = [[1, 1, 1], [1, 1, 0], [1, 1, 1], [1, 1, 1]]
+        zm.zero_matrix_alt(matrix)
+        expected_output = [[1, 1, 0], [0, 0, 0], [1, 1, 0], [1, 1, 0]]
+        self.assertEqual(matrix, expected_output)
+
+        matrix = [[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 0]]
+        zm.zero_matrix_alt(matrix)
+        expected_output = [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]]
+        self.assertEqual(matrix, expected_output)
+
+        matrix = [[0, 1, 1, 1], [1, 0, 1, 1], [1, 1, 0, 1], [1, 1, 1, 0]]
+        zm.zero_matrix_alt(matrix)
+        expected_output = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        self.assertEqual(matrix, expected_output)
+
 
 if __name__ == '__main__':
     unittest.main()
--- Problems/zero_matrix/zero_matrix.py
+++ Problems/zero_matrix/zero_matrix.py
@@ -28,3 +28,60 @@ def zero_matrix(matrix):
         for col in range(n):
             if row in zero_rows or col in zero_cols:
                 matrix[row][col] = 0
+
+
+# Time complexity:  O(mn).
+# Space complexity: O(1).
+def zero_matrix_alt(matrix):
+
+    m = len(matrix)
+    n = len(matrix[0])
+    row_has_zero = False
+    column_has_zero = False
+
+    # Check if the first row has zeroes.
+    for column in range(n):
+        if matrix[0][column] == 0:
+            row_has_zero = True
+            break
+
+    # Check if the first column has zeroes.
+    for row in range(m):
+        if matrix[row][0] == 0:
+            column_has_zero = True
+            break
+
+    # Check the rest of the matrix.
+    for row in range(1, m):
+        for column in range(1, n):
+            if matrix[row][column] == 0:
+                matrix[row][0] = 0;
+                matrix[0][column] = 0;
+
+    # Nullify rows based on values in first column.
+    for row in range(1, m):
+        if matrix[row][0] == 0:
+            nullify_row(matrix, row)
+
+    # Nullify columns based on values in first row.
+    for column in range(1, n):
+        if matrix[0][column] == 0:
+            nullify_column(matrix, column)
+
+    # Nullify the first row.
+    if row_has_zero:
+        nullify_row(matrix, 0)
+
+    # Nullify the first column.
+    if column_has_zero:
+        nullify_column(matrix, 0)
+
+
+def nullify_row(matrix, row):
+    for column in range(len(matrix[0])):
+        matrix[row][column] = 0
+
+
+def nullify_column(matrix, column):
+    for row in range(len(matrix)):
+        matrix[row][column] = 0
