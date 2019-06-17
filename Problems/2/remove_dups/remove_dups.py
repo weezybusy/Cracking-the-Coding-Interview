7c938d8bcbb5b3feca2bdf5727c336da68f0871d
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2019-06-17 19:55:44 +0300

    Added solution from the book and tests for it.

--- Problems/2/remove_dups/remove_dups.py
+++ Problems/2/remove_dups/remove_dups.py
@@ -6,6 +6,8 @@ Remove Dups: Write code to remove duplicates from an unsorted linked list.
 """
 
 
+# Time complexity:  O(n^2).
+# Space complexity: O(1).
 def remove_dups(lst):
 
     if lst.size < 2:
@@ -28,3 +30,19 @@ def remove_dups(lst):
         check = check.next
         prev = check
         curr = check.next
+
+
+# Time complexity:  O(n^2).
+# Space complexity: O(1).
+def remove_dups_alt(lst):
+
+    curr = lst.head
+    while curr is not None:
+        runner = curr
+        while runner.next is not None:
+            if runner.next.data == curr.data:
+                runner.next = runner.next.next
+                lst.size -= 1
+            else:
+                runner = runner.next
+        curr = curr.next
--- Problems/2/remove_dups/test_remove_dups.py
+++ Problems/2/remove_dups/test_remove_dups.py
@@ -3,7 +3,7 @@
 
 import unittest
 from singly_linked_list import Node, SinglyLinkedList
-from remove_dups import remove_dups
+from remove_dups import remove_dups, remove_dups_alt
 
 
 class TestRemoveDups(unittest.TestCase):
@@ -71,6 +71,75 @@ class TestRemoveDups(unittest.TestCase):
         for i in range(len(original_data)):
             lst.insert_tail(Node(original_data[i]))
         remove_dups(lst)
+        self.assertEqual(lst.size, len(expected_data))
+        node = lst.head
+        for i in range(len(expected_data)):
+            self.assertEqual(node.data, expected_data[i])
+            node = node.next
+
+    def test_remove_dups_alt(self):
+
+        # Check removing dups from the empty list.
+        lst = SinglyLinkedList()
+        remove_dups_alt(lst)
+        self.assertEqual(lst.size, 0)
+
+        # Check removing dups from the list wiht one element in it.
+        lst = SinglyLinkedList()
+        lst.insert_tail(Node(1))
+        remove_dups_alt(lst)
+        self.assertEqual(lst.size, 1)
+        self.assertEqual(lst.head.data, 1)
+
+        # Check removing dups from the tree with repeated element in it.
+        lst = SinglyLinkedList()
+        original_data = [1, 1]
+        for i in range(len(original_data)):
+            lst.insert_tail(Node(original_data[i]))
+        remove_dups_alt(lst)
+        self.assertEqual(lst.size, 1)
+        self.assertEqual(lst.head.data, 1)
+
+        lst = SinglyLinkedList()
+        original_data = [1, 1, 1, 1, 1]
+        for i in range(len(original_data)):
+            lst.insert_tail(Node(original_data[i]))
+        remove_dups_alt(lst)
+        self.assertEqual(lst.size, 1)
+        self.assertEqual(lst.head.data, 1)
+
+        # Check removing dups from the tree with mixed data.
+        lst = SinglyLinkedList()
+        original_data = [1, 5, 3, 5, 4, 8, 1, 12, 33, 5]
+        expected_data = [1, 5, 3, 4, 8, 12, 33]
+        for i in range(len(original_data)):
+            lst.insert_tail(Node(original_data[i]))
+        remove_dups_alt(lst)
+        self.assertEqual(lst.size, len(expected_data))
+        node = lst.head
+        for i in range(len(expected_data)):
+            self.assertEqual(node.data, expected_data[i])
+            node = node.next
+
+        lst = SinglyLinkedList()
+        original_data = [38, 49, 51, 1080, 12, -48, -48, 1080]
+        expected_data = [38, 49, 51, 1080, 12, -48]
+        for i in range(len(original_data)):
+            lst.insert_tail(Node(original_data[i]))
+        remove_dups_alt(lst)
+        self.assertEqual(lst.size, len(expected_data))
+        node = lst.head
+        for i in range(len(expected_data)):
+            self.assertEqual(node.data, expected_data[i])
+            node = node.next
+
+        # Check removing dups from the tree with no repeated data.
+        lst = SinglyLinkedList()
+        original_data = [1, 2, 3, 4, 5]
+        expected_data = [1, 2, 3, 4, 5]
+        for i in range(len(original_data)):
+            lst.insert_tail(Node(original_data[i]))
+        remove_dups_alt(lst)
         self.assertEqual(lst.size, len(expected_data))
         node = lst.head
         for i in range(len(expected_data)):
