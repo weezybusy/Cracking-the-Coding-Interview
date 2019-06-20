a359304ff493a223870ebeaa603da6cbb50aa3a2
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2019-06-20 14:24:12 +0300

    Started creating recursive solution.

--- Problems/2/return_kth_to_last/kth_to_last.py
+++ Problems/2/return_kth_to_last/kth_to_last.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 
-from singly_linked_list import Node, SinglyLinkedList
-
-
 """
 Retrun Kth to Last: Implement an alogirightm to find the kth to last element
 of a singly linked list.
 """
+
+
+from singly_linked_list import Node, SinglyLinkedList
 
 
 def kth_to_last(lst, k):
@@ -41,3 +41,24 @@ def kth_to_last_alt(lst, k):
     for i in range(n):
         node = node.next
     return node
+
+
+def kth_to_last_recur(node, k, i):
+
+    if node is None:
+        return None
+
+    tmp_node = kth_to_last_recur(node.next, k, i);
+    i += 1
+    if i == k:
+        return node.data
+    else:
+        return tmp_node
+
+
+lst = SinglyLinkedList()
+for i in range(5):
+    lst.insert_tail(Node(i))
+print(lst)
+
+print(kth_to_last_recur(lst.head, 2, 0))
