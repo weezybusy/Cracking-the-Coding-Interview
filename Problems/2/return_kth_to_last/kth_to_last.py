14162f078a99efee4e77c775ebebd9871081d304
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2019-06-24 12:55:28 +0300

    Removed code with unfinished recursive solution because recursive solution doesn't fit for this problem.

--- Problems/2/return_kth_to_last/kth_to_last.py
+++ Problems/2/return_kth_to_last/kth_to_last.py
@@ -41,37 +41,3 @@ def kth_to_last_alt(lst, k):
     for i in range(n):
         node = node.next
     return node
-
-
-def kth_to_last_recur(node, k):
-    """
-    This solution implies that the singly linked list is implemented without
-    the size variable.
-    """
-    i = getk(node, k, 0)
-    if i < 0:
-        return None
-    while i > 0:
-        node = node.next
-        i -= 1
-    return node
-
-
-def getk(node, k, i):
-    if k < 0:
-        return k
-    elif node.next is None:
-        return i - k
-    else:
-        return getk(node.next, k, i+1)
-
-
-lst = SinglyLinkedList()
-for i in range(5):
-    lst.insert_tail(Node(chr(97+i)))
-print(lst)
-print(f'kth_to_last_recur(lst.head, -1) -> {kth_to_last_recur(lst.head, -1)}')
-print(f'kth_to_last_recur(lst.head, 0) -> {kth_to_last_recur(lst.head, 0)}')
-print(f'kth_to_last_recur(lst.head, 2) -> {kth_to_last_recur(lst.head, 2)}')
-print(f'kth_to_last_recur(lst.head, 4) -> {kth_to_last_recur(lst.head, 4)}')
-print(f'kth_to_last_recur(lst.head, 5) -> {kth_to_last_recur(lst.head, 5)}')
