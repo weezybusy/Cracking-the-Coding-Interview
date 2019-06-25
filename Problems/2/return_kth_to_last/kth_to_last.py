861c7ab49a805ac86974397a0cda71c08435b897
Vitaliy Pisnya <vitaliy.pisnya@gmail.com>
2019-06-25 12:12:34 +0300

    Corrected syntax errors in problem description.

--- Problems/2/return_kth_to_last/kth_to_last.py
+++ Problems/2/return_kth_to_last/kth_to_last.py
@@ -2,7 +2,7 @@
 
 
 """
-Retrun Kth to Last: Implement an alogirightm to find the kth to last element
+Return Kth to Last: Implement an algorithm to find the kth to last element
 of a singly linked list.
 """
 
